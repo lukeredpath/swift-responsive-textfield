781646019d48306a97b356e568b6318dc5ce4f9f
Yi <yqiang@gmail.com>
2025-09-09 13:22:45 -0700

    fix swiftpm warning

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["ResponsiveTextField"]),
     ],
     dependencies: [
-        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.2"),
-        .package(name: "combine-schedulers", url: "https://github.com/pointfreeco/combine-schedulers.git", from: "1.0.2")
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.2"),
+        .package(url: "https://github.com/pointfreeco/combine-schedulers.git", from: "1.0.2")
     ],
     targets: [
         .target(
